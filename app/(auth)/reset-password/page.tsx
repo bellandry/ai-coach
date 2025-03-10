3a2a6d7b9af211448da9207841ca6e778ec1814e
bellandry <bellandry.work@gmail.com>
2025-03-10 12:39:41 +0100

    finally fix forgot password page issues

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,16 +1,15 @@
-/* eslint-disable */
-
 import ResetPasswordForm from "@/components/auth/reset-password-form";
 import { redirect } from "next/navigation";
 
 interface ResetPasswordPageProps {
-  searchParams: { token?: string };
+  params: {};
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default function ResetPasswordPage({
   searchParams,
 }: ResetPasswordPageProps) {
-  const token = searchParams.token;
+  const token = searchParams.token as string | undefined;
 
   if (!token) {
     redirect("/forgot-password");
